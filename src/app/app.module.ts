31454dae2ec3ea9a45d49ef4b8da802e127d0081
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-10-06 15:33:06 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { ModalComponent } from './components/modals/modal/modal.component';
+import { InfoModalComponent } from './components/modals/info-modal/info-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardComponent,
+    ModalComponent,
+    InfoModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/modals/info-modal/info-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fs-info-modal',
+  templateUrl: './info-modal.component.html',
+  styleUrls: ['./info-modal.component.scss']
+})
+export class InfoModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
