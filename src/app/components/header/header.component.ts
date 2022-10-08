b22ba8016177a9b66b3ba40574b60de9fefa4192
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-10-08 10:41:48 +0200

    Create header menu for mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { AppState } from 'src/app/appState';
 import { ModalsService } from 'src/app/shared/services/modals.service';
 import { IUser } from 'src/app/shared/models/User';
 import { ApiService } from 'src/app/shared/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fs-header',
@@ -12,8 +13,9 @@ import { ApiService } from 'src/app/shared/services/api.service';
 })
 export class HeaderComponent implements OnInit {
   user!: IUser;
+  expanded: boolean = false;
 
-  constructor(public modalService: ModalsService, public store: Store<AppState>, public apiService: ApiService) { }
+  constructor(public modalService: ModalsService, public store: Store<AppState>, public apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
       this.store.select(state => state.user).subscribe({
@@ -23,6 +25,12 @@ export class HeaderComponent implements OnInit {
       error: err => {
         console.error(err);
       }
+    })
+  }
+
+  navigate(url: string) {
+    this.router.navigate([url]).then(value => {
+      if(value) this.expanded = false;
     })
   }
 
