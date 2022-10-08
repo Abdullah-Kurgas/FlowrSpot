eda9d9ce84544bfbc46a24c58817b4919879a234
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-10-08 16:42:41 +0200

    Replace fixed data in cards

--- src/app/shared/models/Sighting.ts
+++ src/app/shared/models/Sighting.ts
@@ -1,8 +1,15 @@
+import { IFlower } from "./Flower";
+
 export interface ISighting {
-    id: number;
-    flower_id: number;
-    name: string;
+    comments_count: number;
+    created_at: string;
     description: string;
+    flower: IFlower;
+    id: number;
     latitude: number;
+    likes_count: number;
     longitude: number;
+    name: string;
+    picture: string;
+    user: {id: number, full_name: string};
 }
