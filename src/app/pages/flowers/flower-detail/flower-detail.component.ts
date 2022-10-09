9b200d48aa4f5169e574cac333b8c022a5151043
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-10-09 21:51:20 +0200

    Check favorite icon when is 'true'

--- src/app/pages/flowers/flower-detail/flower-detail.component.ts
+++ src/app/pages/flowers/flower-detail/flower-detail.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/appState';
-import { Flower, IFlower } from 'src/app/shared/models/Flower';
+import { ToggleFavorite } from 'src/app/shared/actions/favoriteAction';
+import { ToggleFlower } from 'src/app/shared/actions/flowerAction';
+import { Flower } from 'src/app/shared/models/Flower';
 import { FlowerService } from 'src/app/shared/services/flower.service';
+import { Utils } from 'src/app/shared/Utils';
 
 @Component({
   selector: 'fs-flower-detail',
@@ -12,6 +15,8 @@ import { FlowerService } from 'src/app/shared/services/flower.service';
 })
 export class FlowerDetailComponent implements OnInit {
   flower: Flower = new Flower();
+
+  utils = Utils;
 
   constructor(private flowerService: FlowerService, private store: Store<AppState>, public route: ActivatedRoute) { }
 
@@ -23,6 +28,27 @@ export class FlowerDetailComponent implements OnInit {
       error: err => console.error(err)
 
     })
+  }
+
+  toggleFavorite(flower: any) {
+    if (this.utils.getDataFromInsideObj(flower, 'favorite')) {
+      this.flowerService.removeFromFavoriteList(flower.id, this.utils.getDataFromInsideObj(flower, 'id')).subscribe({
+        next: res => {
+          this.store.dispatch(new ToggleFavorite(flower));
+        },
+        error: err => console.error(err)
+      })
+      return;
+    }
+
+    if (!flower.favorite) {
+      this.flowerService.addToFavoriteList(flower.id).subscribe({
+        next: (res) => {
+          this.store.dispatch(new ToggleFlower(flower));
+        },
+        error: err => console.error(err)
+      })
+    }
   }
 
 }
